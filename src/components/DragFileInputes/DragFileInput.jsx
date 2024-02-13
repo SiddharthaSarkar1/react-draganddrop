95f8c92af29928eea47136bfe2f8dfdc3c400faf
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-02-14 00:15:17 +0530

    drang and drop functionality implemented frontend

--- src/components/DragFileInputes/DragFileInput.jsx
+++ src/components/DragFileInputes/DragFileInput.jsx
@@ -1,20 +1,75 @@
-import React from 'react';
-import { ImageConfig } from '../../config/ImageConfig';
-import uploadImg from '../../assets/cloud-upload-regular-240.png';
-
+import React, { useRef, useState } from "react";
+import { X } from 'lucide-react';
+import { ImageConfig } from "../../config/ImageConfig";
+import uploadImg from "../../assets/cloud-upload-regular-240.png";
 
 import "./DragFileInput.css";
 
 const DragFileInput = () => {
+  const wrapperRef = useRef(null);
+
+  const [fileList, setFileList] = useState([]);
+
+  const onDragEnter = () => wrapperRef.current.classList.add("dragover");
+  const onDragLeave = () => wrapperRef.current.classList.remove("dragover");
+  const onDrop = () => wrapperRef.current.classList.remove("dragover");
+
+  const onFileDrop = (e) => {
+    const newFile = e.target.files[0];
+    if (newFile) {
+      const updatedList = [...fileList, newFile];
+      setFileList(updatedList);
+    }
+  };
+
+  const fileRemove = (file) => {
+    const updatedList = [...fileList];
+    updatedList.splice(fileList.indexOf(file), 1);
+    setFileList(updatedList);
+  };
+
   return (
-    <div className='drop-file-input'>
-      <div className="drop-file-input__label">
-        <img src={uploadImg} alt="upload Image" />
-        <p>Drag & Drop your files here</p>
+    <>
+      <div
+        className="drop-file-input"
+        ref={wrapperRef}
+        onDragEnter={onDragEnter}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
+        <div className="drop-file-input__label">
+          <img src={uploadImg} alt="upload Image" />
+          <p>Drag & Drop your files here</p>
+        </div>
+        <input type="file" value="" onChange={onFileDrop} />
       </div>
-      <input type="file" />
-    </div>
-  )
-}
+      {fileList.length > 0 ? (
+        <div className="drop-file-preview">
+          <p className="drop-file-preview__title">Ready to upload</p>
+          {fileList.map((item, index) => (
+            <div key={index} className="drop-file-preview__item">
+              <img
+                src={
+                  ImageConfig[item.type.split("/")[1]] || ImageConfig["default"]
+                }
+                alt=""
+              />
+              <div className="drop-file-preview__item__info">
+                <p>{item.name}</p>
+                <p>{item.size}B</p>
+              </div>
+              <span
+                className="drop-file-preview__item__del"
+                onClick={() => fileRemove(item)}
+              >
+                <X />
+              </span>
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 export default DragFileInput;
